f24646635f7ed151905568ac33d44ae4e22b1c01
Ali-Elchab <alielchab01@gmail.com>
2025-06-20 01:00:33 +0300

    change dashboard name and logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,8 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('Servio Admin')
+            ->favicon(asset('favicon.png'))
             ->login()
             ->colors([
                 'primary' => Color::Amber,
